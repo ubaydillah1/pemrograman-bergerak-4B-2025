2f492f76b37faf95ac1c3bb869bd06b47b14577a
ubaydillah1 <ubaydillah1737@gmail.com>
2025-03-19 09:56:15 +0700

    KakKukuh_230441100052_UbayDillah_Submission

--- tugas-dart/pember-dart-code-submission/bin/exam1/exam1.dart
+++ tugas-dart/pember-dart-code-submission/bin/exam1/exam1.dart
@@ -1,9 +1,9 @@
 dynamic studentInfo() {
   // TODO 1
 
-  var name;
-  var favNumber;
-  var isPraktikan;
+  var name = "Ubay Dillah";
+  var favNumber = 7;
+  var isPraktikan = true;
 
   // End of TODO 1
   return [name, favNumber, isPraktikan];
@@ -13,20 +13,24 @@ dynamic circleArea(num r) {
   if (r < 0) {
     return 0.0;
   } else {
-    const double pi = 3.1415926535897932; //π sama dengan library dart.math;
+    const double pi = 3.1415926535897932;
 
-    // TODO 2
-
-    return 0;
+    double result = pi * r * r;
+    return result;
 
     // End of TODO 2
   }
 }
 
 int? parseAndAddOne(String? input) {
-  // TODO 3
+  if (input == null) {
+    return null;
+  }
 
-  return 0;
-
-  // End of TODO 3
+  try {
+    int parsedValue = int.parse(input);
+    return parsedValue + 1;
+  } catch (e) {
+    throw Exception('Input harus berupa angka');
+  }
 }
--- tugas-dart/pember-dart-code-submission/bin/exam3/exam3.dart
+++ tugas-dart/pember-dart-code-submission/bin/exam3/exam3.dart
@@ -1,25 +1,21 @@
 Set uniqueElement(List<int> myList) {
-  // TODO 1
-
-  return {};
-
-  // End of TODO 1
+  return myList.toSet();
 }
 
 Map<String, String> buildFutsalPlayersMap() {
-  // TODO 2
-
-  return {};
-
-  // End of TODO 2
+  return {
+    'Goalkeeper': 'Andi',
+    'Anchor': 'Irfan',
+    'Pivot': 'Fikri',
+    'Right Flank': 'Ubay',
+    'Left Flank': 'Hafiz',
+  };
 }
 
 Map<String, String> updatePivotPlayer() {
   final futsalPlayers = buildFutsalPlayersMap();
 
-  // TODO 3
-
-  // End of TODO 3
+  futsalPlayers['Pivot'] = 'Dillah';
 
   return futsalPlayers;
 }
